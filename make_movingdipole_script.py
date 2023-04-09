92c0a17fa5b379bd61ccd88c6d7a1cd84b2bc453
Aaron Geller <aaron.s.geller@gmail.com>
2023-04-09 07:05:47 -0600

    Added option to do EEG dipole and modified output names.

--- make_movingdipole_script.py
+++ make_movingdipole_script.py
@@ -8,7 +8,7 @@ import numpy as np
 # see http://wiki.besa.de/index.php?title=Moving_Dipole_Fit
 
 if len(sys.argv) < 4:
-    print("Usage: python make_movingdipole_script.py <start_ms> <spacing_ms> <patientnum> [fnamestem] [numdipoles]")
+    print("Usage: python make_movingdipole_script.py <start_ms> <spacing_ms> <patientnum> [MEGorEEG] [fnamestem] [numdipoles]")
     quit()
     
 start_offset_ms = int(sys.argv[1])
@@ -20,13 +20,19 @@ if not os.path.isdir(output_path):
     os.makedirs(output_path)
 
 if len(sys.argv)>4:
-    fname_stem = sys.argv[4]
+    eeg_or_meg = sys.argv[4]
 else:
-    fname_stem = "movingdipole"
-fname = os.path.join(output_path, fname_stem + "_" + patient_num + ".bbat")
+    eeg_or_meg = "GRA" # default to MEG gradiometers, pass "EEG" for EEG
 
 if len(sys.argv)>5:
-    num_dipoles = int(sys.argv[5])
+    fname_stem = sys.argv[5]
+else:
+    fname_stem = "movingdipole"
+fname_fullstem = os.path.join(output_path, fname_stem + "_" + patient_num + "_" + eeg_or_meg)
+bbat_fname = fname_fullstem + ".bbat"
+
+if len(sys.argv)>6:
+    num_dipoles = int(sys.argv[6])
 else:
     num_dipoles = 11
 
@@ -55,9 +61,9 @@ for i in range(num_dipoles):
 
     decimal_colors.append(blue_dec*(2**16) + green_dec*(2**8) + red_dec)
     
-part1 = "MAINFilter(LC:4.00-6dB-f,HC:40.00-24dB-z,NF:off,BP:off)\nMAINMarkBlock(WholeSegment,-,1,SendToSA)\nSAsetDefaultSourceType(Dipole)\nSAchannelTypeForFit(GRA)\nSAFitConstraint(RVOn:1.00,ENOff:1.00,MDOn:1.00,IMOff:1.00)\nSAaddSource(Dipole,UnitSphere,0.000,0.000,0.000,0.000,0.000,1.000,-," + str(decimal_colors[0]) + ",FitEnable,FitDisableOtherSources,BR29.bsa,1.00,30.00,Noise_)\nSAsetOrActivateSource(1,Disable)\nSAHeadModel(0)\nSAsetOrActivateSource(1,Enable)\nSAsetCursor(" + str(start_offset_ms) + ",NoDrawMap)\nSAfit(Sources)\nSAsetOrActivateSource(Last,Off)\n"
+part1 = "MAINFilter(LC:4.00-6dB-f,HC:40.00-24dB-z,NF:off,BP:off)\nMAINMarkBlock(WholeSegment,-,1,SendToSA)\nSAsetDefaultSourceType(Dipole)\nSAchannelTypeForFit(" + eeg_or_meg + ")\nSAFitConstraint(RVOn:1.00,ENOff:1.00,MDOn:1.00,IMOff:1.00)\nSAaddSource(Dipole,UnitSphere,0.000,0.000,0.000,0.000,0.000,1.000,-," + str(decimal_colors[0]) + ",FitEnable,FitDisableOtherSources,BR29.bsa,1.00,30.00,Noise_)\nSAsetOrActivateSource(1,Disable)\nSAHeadModel(0)\nSAsetOrActivateSource(1,Enable)\nSAsetCursor(" + str(start_offset_ms) + ",NoDrawMap)\nSAfit(Sources)\nSAsetOrActivateSource(Last,Off)\n"
 
-of = open(fname, 'w')
+of = open(bbat_fname, 'w')
 of.write(part1)
 
 for i in range(num_dipoles-1):
@@ -68,4 +74,4 @@ part3 = "SAsetOrActivateSource(All,On)\nSAfitInterval(" + str(start_offset_ms) +
 of.write(part3)
 
 of.close()
-plt.savefig(os.path.join(output_path, fname_stem + "_" + patient_num + ".png"))
+plt.savefig(os.path.join(output_path, fname_fullstem + "_timefig.png"))
